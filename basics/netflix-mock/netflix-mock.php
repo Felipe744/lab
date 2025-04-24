f0dbd9445b6d955adaa4574f5f2f9af6ba65b0da
Felipe744 <felipe_oliveira74@hotmail.com>
2025-04-24 20:30:16 -0300

    commit basics, trabalhando com strings

--- basics/netflix-mock/netflix-mock.php
+++ basics/netflix-mock/netflix-mock.php
@@ -1,10 +1,15 @@
 <?php
 
-echo "Hello world";
-
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
 $notaFilme = 8.8;
 $incluidoNoPlano = true;
 
-echo $nomeFilme;
+echo "Nome do filme: " . $nomeFilme . "\n"; //concatenação
+echo "Nome do filme: $nomeFilme\n"; //interpolação
+
+//sem vantagens de usar aspas duplas ou aspas simples, usar convenção do time!
+
+$testeVariavelInput = $argv[1] ?? 2022;
+
+echo "Teste variável: " . $testeVariavelInput . "\n";
