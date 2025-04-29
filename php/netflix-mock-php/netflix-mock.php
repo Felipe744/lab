3b83cbebeb982178763c4318f51067fdb1a7c14b
Felipe744 <felipe_oliveira74@hotmail.com>
2025-04-28 21:51:31 -0300

    parte um do curso de php finalizada

--- php/netflix-mock-php/netflix-mock.php
+++ php/netflix-mock-php/netflix-mock.php
@@ -1,6 +1,9 @@
 <?php
 
 $nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Filme2";
+$nomeFilme = "Filme3";
+
 $anoLancamento = 2022;
 $notaFilme = 8.8;
 $incluidoNoPlano = true;
@@ -13,3 +16,55 @@ echo "Nome do filme: $nomeFilme\n"; //interpolação
 $testeVariavelInput = $argv[1] ?? 2022;
 
 echo "Teste variável: " . $testeVariavelInput . "\n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme eh um lancamento\n";
+} else {
+    echo "Nao eh um lancamento\n";
+}
+
+$genero = match($nomeFilme) {
+    "Top Gun - Maverick" => "acao",
+    "Filme2" => "terror",
+    "Filme3" => "comedia",
+    default => "outros",
+};
+
+echo $genero . "\n";
+
+$filme = [
+    "nome" => "Filme1",
+    "ano" => 2020,
+    "nota" => 9.5,
+    "genero" => "comedia",
+];
+
+echo $filme["nome"] . "\n";
+
+$notas = [];
+
+for ($contador = 0; $contador < 10; $contador++) {
+    $notas[] = $contador;
+}
+
+var_dump($notas);
+
+echo "\n" . count($notas);
+
+$somaDeNotas = 0;
+
+foreach ($notas as $nota) {
+    $somaDeNotas += $nota;
+}
+
+echo "\n Soma das notas = $somaDeNotas";
+
+$somaDeNotas = array_sum($notas);
+
+echo "\n Soma das notas = $somaDeNotas";
+
+$numeroTeste = 1_000; //posso add o _ apenas para melhorar a visualizacao
+
+$numeroTeste = (int) 1.6;
+
+echo "\n $numeroTeste";
